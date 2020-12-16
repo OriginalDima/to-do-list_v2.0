bcd658d561d2ae5a4df8a057ee7c0f93b73abf59
Dmytro <dmytroresler@gmail.com>
2020-12-16 17:55:57 +0100

    post-list and post-list-items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: 'Im learning React', important: true, id:'qwqwq'},
+        {label: 'Im still learning React', important: false, id:'asasa'},
+        {label: 'Calm down, need some time', important: false, id:'zxzxz'}
+    ];
+
     return (
         <div className='app'>
             <AppHeader/>
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data} />
             <PostAddForm/>
         </div>
     )
